109697444a9e1409182522df5293f422a34a72de
Richard Savard <richard.savard.ca@gmail.com>
2018-03-28 21:19:29 -0400

    Prototype of class

--- main.py
+++ main.py
@@ -2,66 +2,52 @@
 # Create by: Richard Savard
 
 from __future__ import unicode_literals
-from abc import ABC
 
 
-class BaseNode(ABC):
-    """Red-black tree base node"""
+class NodeItem(object):
 
+    """Red-black tree node"""
 
-
-class Node(BaseNode):
-    """Red-black tree string node"""
-    
     def __init__(self, key, value=None):
-        self.khash = khash
         self.key = key
         self.value = value
         self.red = False
         self.right = None
         self.left = None
+        print("NodeItem({1}) => {0}".format(self._key, self._hash))
 
+    @property
+    def key(self):
+        return self._key
 
-    def compareTo(self, key):
-        return -1
+    @key.setter
+    def key(self, key):
+        """Set the key value"""
+        self._hash = self.hashing(key)
+        self._key = key
 
-        
-    @methodestatic
+    @staticmethod
     def hashing(key):
-        """Get the int hash value of a string"""
-        modulo = 0xFFFFFFFB
-        myhash = 0
-        for i, ch in enumarate(bytearray(key.encode('utf-8'))):
-            myhash = (31 * myhash + ch) % modulo
-            if i >= 1000:
-                break
-        return myhash
+        return hash(key)
 
 
 class RedBlackTree(object):
 
     """Red-black tree object"""
 
-    def __init__(self):
-        self._znode = Node()
+    def __init__(self, node_type):
+        self._node_item = node_type
+        self._znode = None
         self._head = self._znode
         self._count = 0
-        if hashing:
-            self._hashing = hashing
-        else:
-            self._hashing = lambda x: x
-
-        if compare:
-            self._compare = compare
-        else:
-            self._compare = lambda a, b: -1 if a < b else int(a > b)
 
     def count(self):
         return self._count
 
     def put(self, key, value):
-        khash = self._hashing(key)
-
+        mynode = self._node_item(key)
+        
+        """
         node = self._head
         pnode = gpnode = ggpnode = node
         while node is not self._znode:
@@ -69,22 +55,22 @@ class RedBlackTree(object):
             gpnode = pnode
             pnode = node
 
-            if khash < node.khash or (khash == node.khash and
-                                      self._compare(key, node.key) < 0):
+            if node.key < key:
                 node = node.left
             else:
                 node = node.right
 
             if node.left.red and node.right.red:
-                self._split(khash, key, ggpnode, gpnode, pnode, node)
+                self._split()
 
-        mynode = Node(khash, key)
+        mynode = self._node_item(key)
         mynode.value = value
         mynode.left = self._znode
         mynode.right = self._znode
-        self._split(khash, key, ggpnode, gpnode, pnode, mynode)
+        self._split()
 
         self._size += 1
+        """
         return True
 
     def pop(self, key):
@@ -93,17 +79,29 @@ class RedBlackTree(object):
     def search(self, key):
         return "value"
 
-    def update(self, node, value):
+    def update(self, key, value):
         return "old value"
 
-    def _rotate(self, khash, key, node):
+    def _rotate(self):
         pass
 
-    def _split(self, khash, key, ggpnode, gpnode, pnode, node):
+    def _split(self):
         pass
 
 
 if __name__ == '__main__':
-    t = RedBlackTree()
+    t = RedBlackTree(NodeItem)
     t.put('salut', 'valeur de salut')
     t.put('hello', 'valeur de hello')
+
+
+# @staticmethod
+# def hashing(key):
+#    """Get the int hash value of a string"""
+#    modulo = 0xFFFFFFFB
+#    myhash = 0
+#    for i, ch in enumerate(bytearray(key.encode('utf-8'))):
+#        myhash = (31 * myhash + ch) % modulo
+#        if i >= 1000:
+#            break
+#    return myhash
